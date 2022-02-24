181cc2df7fd5c5661a0976efd04119fe4c97a9c2
Daniel Bird <danieldbird@gmail.com>
2022-02-25 12:04:20 +1300

    add pseudocode of basic functionality

--- script.js
+++ script.js
@@ -1,1 +1,26 @@
-//
+// container for the clculator
+
+// number display area
+// running keydowns
+// result of calculation, showing last number pressed, until you press equals "="
+// when equals is pressed, the calculation goes up, and result is displayed below
+
+// number pad
+// clear display button
+// operators
+// number pad
+
+// check if a number or decimal was pressed, or an operator, or equals, or AC.
+
+// a possible array of objects containing
+// [{
+//  class: ""
+//  type: "operator"
+//  value: ""
+// }]
+
+// 20 buttons produced now.
+
+// function to clear display
+
+// function to operate on 2 numbers + - * /
