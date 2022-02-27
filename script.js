e78b8c5bba6364b8f7fe5ccece9f0a72370cba8a
Daniel Bird <danieldbird@gmail.com>
2022-02-28 09:50:14 +1300

    add button event listeners

--- script.js
+++ script.js
@@ -1,129 +1,72 @@
-// container for the clculator
-
 // number display area
-// running keydowns
 // result of calculation, showing last number pressed, until you press equals "="
 // when equals is pressed, the calculation goes up, and result is displayed below
 
-// number pad
-// clear display button
-// operators
-// number pad
+const display = document.querySelector(".display");
+const numpad = document.querySelector(".numpad");
 
-// check if a number or decimal was pressed, or an operator, or equals, or AC.
+const keys = ["AC", "/", "x", 7, 8, 9, "-", 4, 5, 6, "+", 1, 2, 3, "=", 0, "."];
+const operators = [1, 2, 6, 10];
+const functions = [0, 14];
 
-// a possible array of objects containing
-// [{
-//  class: ""
-//  type: "operator"
-//  value: ""
-// }]
+// let currentDisplayValue = 0;
+// let currentDisplayHistory = 0;
 
-// 20 buttons produced now.
+// add buttons
+function addButtons() {
+  for (let i = 0; i < keys.length; i++) {
+    const el = document.createElement("div");
+    el.innerHTML = keys[i];
+    el.classList.add("key");
+    // add operator classes
+    if (operators.includes(i)) {
+      el.classList.add("operator");
+    }
+    // add function classes
+    if (functions.includes(i)) {
+      el.classList.add("function");
+    }
 
-// function to clear display
+    // add ac class and event listener
+    if (i === 0) {
+      el.classList.add("ac");
+      el.addEventListener("click", pressAC);
+    }
 
-// function to operate on 2 numbers + - * /
+    // add zero class and event listener
+    else if (i === 15) {
+      el.classList.add("zero");
+      el.addEventListener("click", pressNumber);
+    }
 
-const displayLive = document.querySelector(".displayLive");
-const displayHistory = document.querySelector(".displayHistory");
-// const displayLive = document.querySelector(".displayLive");
+    // add equal class and event listener
+    else if (i === 14) {
+      el.classList.add("equal");
+      el.addEventListener("click", pressEquals);
+    } else {
+      el.addEventListener("click", pressNumber);
+    }
 
-let currentDisplayValue = 0;
-let currentDisplayHistory = 0;
-
-let keys = [];
-
-function keyPad(id, className, value, type) {
-  this.id = id;
-  this.class = className;
-  this.value = value;
-  this.type = type;
-}
-
-let keyBoards = document.getElementsByClassName("key");
-
-for (let i = 0; i < keyBoards.length; i++) {
-  let test = new keyPad(keyBoards[i].id, "key", keyBoards[i].innerHTML, "number");
-  keys.push(test);
-}
-
-const operators = [0, 14];
-
-for (let i = 0; i < operators.length; i++) {
-  keys[operators[i]].type = "operator";
-}
-
-console.log(keys);
-
-displayLive.innerHTML = currentDisplayValue;
-displayHistory.innerHTML = currentDisplayHistory;
-
-console.log(displayLive);
-
-for (let i = 0; i < keys.length; i++) {
-  if (keys[i].type === "number") {
-    document.getElementById(keys[i].id).addEventListener("click", clickNumber);
+    numpad.appendChild(el);
   }
 }
 
-function clickNumber() {
-  console.log("number clicked!");
+// function to clear display
+function pressAC() {
+  console.log("Pressed AC");
 }
 
-// const keysId = ['ac', 'divide', 'multiplier', 'seven', 'eight', 'nine', 'minus', 'four', 'five', 'six',]
-// const keysValue = ['AC', '/', 'x', 7, 8, 9, '-', 4, 5, 6, '+', 1, 2, 3, '=', 0, '.']
+function pressNumber(i) {
+  console.log(i.target.innerHTML);
+}
 
-// let keys = [
-//   {
-//     value: "AC",
-//   },
-//   {
-//     value: "/",
-//   },
-//   {
-//     value: "X",
-//   },
-//   {
-//     value: "7",
-//   },
-//   {
-//     value: "8",
-//   },
-//   {
-//     value: "9",
-//   },
-//   {
-//     value: "-",
-//   },
-//   {
-//     value: "4",
-//   },
-//   {
-//     value: "5",
-//   },
-//   {
-//     value: "6",
-//   },
-//   {
-//     value: "+",
-//   },
-//   {
-//     value: "1",
-//   },
-//   {
-//     value: "2",
-//   },
-//   {
-//     value: "3",
-//   },
-//   {
-//     value: "=",
-//   },
-//   {
-//     value: "0",
-//   },
-//   {
-//     value: ".",
-//   },
-// ];
+// function to operate on 2 numbers + - * /
+function pressEquals(i) {
+  console.log(i.target.innerHTML);
+}
+
+function start() {
+  addButtons();
+}
+
+start();
